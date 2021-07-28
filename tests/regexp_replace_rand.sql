876c425307e030608455a5567f74b76482134f60
Michael Furmur <furmur@pm.me>
2021-07-28 03:59:08 +0300

    rewrite tests

--- tests/regexp_replace_rand.sql
+++ tests/regexp_replace_rand.sql
@@ -1,72 +1,57 @@
+set search_path TO 'yeti_ext';
+
+do language plpgsql $$
+declare
+    -- tests cases: [description, sql, expected_result]
+    -- '-' in expected result field means skip result checking
+    tests text[] := array[
 /*******************************
  * regexp_replace_rand(TEXT,TEXT,TEXT) tests *
  *******************************
-
  */
-
--- valid regexp_replace, with replace_rand
-select regexp_replace_rand('a','a','br(4)');
--- valid regexp_replace, no replace_rand
-select regexp_replace_rand('a','a','b');
-
--- regexp_replace exception. no replace_rand
-select regexp_replace_rand('a','a[','b');
--- regexp_replace exception. with replace_rand
-select regexp_replace_rand('a','a[','br(4)');
-
--- NULL in
-select regexp_replace_rand(NULL,'a','b');
--- empty in
-select regexp_replace_rand('','a','b');
-
--- NULL rule
-select regexp_replace_rand('a',NULL,'b');
--- empty rule
-select regexp_replace_rand('a','','b');
-
--- NULL result
-select regexp_replace_rand('a','a',NULL);
--- empty result
-select regexp_replace_rand('a','q','');
--- empty regexp_replace result
-select regexp_replace_rand('a','a','');
-
+        ['valid regexp_replace, no replace_rand',E'regexp_replace_rand(\'a\',\'a\',\'b\')','b'],
+        ['valid regexp_replace, with replace_rand',E'regexp_replace_rand(\'a\',\'a\',\'br(4)\')','-'],
+        ['regexp_replace exception. no replace_rand',E'regexp_replace_rand(\'a\',\'a[\',\'b\')','a'],
+        ['regexp_replace exception. with replace_rand',E'regexp_replace_rand(\'a\',\'a[\',\'br(4)\')','a'],
+        ['NULL in',E'regexp_replace_rand(null,\'a\',\'b\')',null],
+        ['empty in',E'regexp_replace_rand(\'\',\'a\',\'b\')',''],
+        ['NULL rule',E'regexp_replace_rand(\'a\',null,\'b\')','a'],
+        ['empty rule',E'regexp_replace_rand(\'a\',\'\',\'b\')','a'],
+        ['NULL result',E'regexp_replace_rand(\'a\',\'a\',null)','a'],
+        ['empty result',E'regexp_replace_rand(\'a\',\'q\',\'\')','a'],
+        ['empty regexp_replace result',E'regexp_replace_rand(\'a\',\'a\',\'\')','a'],
+-- table tests
+        ['empty rule in table', E'regexp_replace_rand(arg,rule,result) from t', 'a'],
 /**************************************************
  * regexp_replace_rand(TEXT,TEXT,TEXT,TEXT) tests *
  **************************************************
-
  */
+        ['valid regexp_replace, with replace_rand',E'regexp_replace_rand(\'a\',\'a\',\'br(4)\',\'g\')','-'],
+        ['valid regexp_replace, no replace_rand',E'regexp_replace_rand(\'a\',\'a\',\'b\',\'g\')','b'],
+        ['regexp_replace exception. no replace_rand',E'regexp_replace_rand(\'a\',\'a[\',\'b\',\'g\')','a'],
+        ['regexp_replace exception. with replace_rand',E'regexp_replace_rand(\'a\',\'a[\',\'br(4)\',\'g\')','a'],
+        ['NULL in',E'regexp_replace_rand(null,\'a\',\'b\',\'g\')',null],
+        ['empty in',E'regexp_replace_rand(\'\',\'a\',\'b\',\'g\')',''],
+        ['NULL rule',E'regexp_replace_rand(\'a\',null,\'b\',\'g\')','a'],
+        ['empty rule',E'regexp_replace_rand(\'a\',\'\',\'b\',\'g\')','a'],
+        ['NULL result',E'regexp_replace_rand(\'a\',\'a\',null,\'g\')','a'],
+        ['empty result',E'regexp_replace_rand(\'a\',\'q\',\'\',\'g\')','a'],
+        ['empty regexp_replace result',E'regexp_replace_rand(\'a\',\'a\',\'\',\'g\')','a'],
+        ['NULL opt',E'regexp_replace_rand(\'a\',\'a\',\'b\',null)',null],
+        ['empty opt',E'regexp_replace_rand(\'a\',\'a\',\'b\',\'\')','b']
+    ];
+    v text[];
+    ret text;
+begin
+    create temporary table t(arg text, rule text, result text) on commit drop;
+    insert into t VALUES('a','','b');
 
--- valid regexp_replace, with replace_rand
-select regexp_replace_rand('a','a','br(4)','g');
--- valid regexp_replace, no replace_rand
-select regexp_replace_rand('a','a','b','g');
-
--- regexp_replace exception. no replace_rand
-select regexp_replace_rand('a','a[','b','g');
--- regexp_replace exception. with replace_rand
-select regexp_replace_rand('a','a[','br(4)','g');
-
--- NULL in
-select regexp_replace_rand(NULL,'a','b','g');
--- empty in
-select regexp_replace_rand('','a','b','g');
-
--- NULL rule
-select regexp_replace_rand('a',NULL,'b','g');
--- empty rule
-select regexp_replace_rand('a','','b','g');
-
--- NULL result
-select regexp_replace_rand('a','a',NULL,'g');
--- empty result
-select regexp_replace_rand('a','q','','g');
--- empty regexp_replace result
-select regexp_replace_rand('a','a','','g');
-
--- NULL opt
-select regexp_replace_rand('a','a','b',NULL);
-
--- empty opt
-select regexp_replace_rand('a','a','b','');
-
+    <<"test loop">>
+    foreach v slice 1 in array tests loop
+        execute 'select ' || v[2] into ret;
+        raise notice '% -> "%" (expected "%") //%', v[2],ret,v[3],v[1];
+        if ((v[3] is null) or v[3]!='-') and (((v[3] is null) != (ret is null)) or v[3]!=ret) then
+            raise exception 'got "%" while expected "%" for the last test case', ret, v[3];
+        end if;
+    end loop "test loop";
+end $$;
