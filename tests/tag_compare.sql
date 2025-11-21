8edc9535910add50dd4600565920ae5ac2483865
Michael Furmur <furmur@pm.me>
2025-11-21 22:16:19 +0200

    tag_compare: update tests for mode:2

--- tests/tag_compare.sql
+++ tests/tag_compare.sql
@@ -1,3 +1,4 @@
+-- tests are written to meet expected values tables here: https://yeti-switch.org/documentation/best-practices/using-routing-tags.html
 BEGIN;
 
 -- CREATE EXTENSION IF NOT EXISTS yeti WITH SCHEMA yeti_ext;
@@ -121,10 +122,61 @@ CREATE FUNCTION test_tags_compare_mutual() RETURNS SETOF TEXT AS $$ BEGIN
     RETURN NEXT is(tag_compare(array[NULL]::integer[], array[2], 1::smallint),           1, 'any_2');
 END; $$ LANGUAGE plpgsql;
 
--- B_IN_A
-CREATE FUNCTION test_tags_compare_a_in_b() RETURNS SETOF TEXT AS $$ BEGIN
-    -- mode 2 is identical to mode 0 but swaps a,b parameters internally. so it's enough to check one basic op
-    RETURN NEXT is(tag_compare(array[1], array[1,2], 2::smallint), 2, '1_12');
+-- INTERSECTION
+CREATE FUNCTION test_tags_compare_intersection() RETURNS SETOF TEXT AS $$ BEGIN
+    -- a: notag
+    RETURN NEXT is(tag_compare(array[]::integer[], array[]::integer[], 2::smallint), 3, 'notags_notags');
+    RETURN NEXT is(tag_compare(array[]::integer[], array[1], 2::smallint),           0, 'notags_1');
+    RETURN NEXT is(tag_compare(array[]::integer[], array[1,2], 2::smallint),         0, 'notags_12');
+    RETURN NEXT is(tag_compare(array[]::integer[], array[2], 2::smallint),           0, 'notags_2');
+
+    -- a: 1
+    RETURN NEXT is(tag_compare(array[1], array[]::integer[], 2::smallint), 0, '1_notags');
+    RETURN NEXT is(tag_compare(array[1], array[1], 2::smallint),           3, '1_1');
+    RETURN NEXT is(tag_compare(array[1], array[1,2], 2::smallint),         3, '1_12');
+    RETURN NEXT is(tag_compare(array[1], array[2], 2::smallint),           0, '1_2');
+
+    -- a: 1,2
+    RETURN NEXT is(tag_compare(array[1,2], array[]::integer[], 2::smallint), 0, '12_notags');
+    RETURN NEXT is(tag_compare(array[1,2], array[1], 2::smallint),           3, '12_1');
+    RETURN NEXT is(tag_compare(array[1,2], array[1,2], 2::smallint),         3, '12_12');
+    RETURN NEXT is(tag_compare(array[1,2], array[2], 2::smallint),           3, '12_2');
+
+    -- a: 1,2,any
+    RETURN NEXT is(tag_compare(array[1,2,NULL], array[]::integer[], 2::smallint), 0, '12any_notag');
+    RETURN NEXT is(tag_compare(array[1,2,NULL], array[1], 2::smallint),           3, '12any_1');
+    RETURN NEXT is(tag_compare(array[1,2,NULL], array[1,2], 2::smallint),         3, '12any_12');
+    RETURN NEXT is(tag_compare(array[1,2,NULL], array[2], 2::smallint),           3, '12any_2');
+
+    -- a: 1,3
+    RETURN NEXT is(tag_compare(array[1,3], array[]::integer[], 2::smallint), 0, '13_notags');
+    RETURN NEXT is(tag_compare(array[1,3], array[1], 2::smallint),           3, '13_1');
+    RETURN NEXT is(tag_compare(array[1,3], array[1,2], 2::smallint),         3, '13_12');
+    RETURN NEXT is(tag_compare(array[1,3], array[2], 2::smallint),           0, '13_2');
+
+    -- a: 1,any
+    RETURN NEXT is(tag_compare(array[1,NULL], array[]::integer[], 2::smallint), 0, '1any_notags');
+    RETURN NEXT is(tag_compare(array[1,NULL], array[1], 2::smallint),           3, '1any_1');
+    RETURN NEXT is(tag_compare(array[1,NULL], array[1,2], 2::smallint),         3, '1any_12');
+    RETURN NEXT is(tag_compare(array[1,NULL], array[2], 2::smallint),           1, '1any_2');
+
+    -- a: 2
+    RETURN NEXT is(tag_compare(array[2], array[]::integer[], 2::smallint), 0, '2_notags');
+    RETURN NEXT is(tag_compare(array[2], array[1], 2::smallint),           0, '2_1');
+    RETURN NEXT is(tag_compare(array[2], array[1,2], 2::smallint),         3, '2_12');
+    RETURN NEXT is(tag_compare(array[2], array[2], 2::smallint),           3, '2_2');
+
+    -- a: 2,any
+    RETURN NEXT is(tag_compare(array[2,NULL], array[]::integer[], 2::smallint), 0, '2any_notags');
+    RETURN NEXT is(tag_compare(array[2,NULL], array[1], 2::smallint),           1, '2any_1');
+    RETURN NEXT is(tag_compare(array[2,NULL], array[1,2], 2::smallint),         3, '2any_12');
+    RETURN NEXT is(tag_compare(array[2,NULL], array[2], 2::smallint),           3, '2any_2');
+
+    -- any
+    RETURN NEXT is(tag_compare(array[NULL]::integer[], array[]::integer[], 2::smallint), 0, 'any_notags');
+    RETURN NEXT is(tag_compare(array[NULL]::integer[], array[1], 2::smallint),           1, 'any_1');
+    RETURN NEXT is(tag_compare(array[NULL]::integer[], array[1,2], 2::smallint),         1, 'any_12');
+    RETURN NEXT is(tag_compare(array[NULL]::integer[], array[2], 2::smallint),           1, 'any_2');
 END; $$ LANGUAGE plpgsql;
 
 SELECT * FROM runtests();
